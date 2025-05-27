03653aef431428e74d56a70b5ef0a0013a5971da
Emeka Egwim <egwim.emeka@gmail.com>
2025-05-27 01:25:16 -0600

    Sliplane Container Deploy - Client

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,11 +8,13 @@ export default defineConfig({
   preview: {
   port: 3000,
   strictPort: true,
+  allowedHosts: ['chimaira-client.sliplane.app']
  },
   server: {
     port: 3000,
     strictPort: true,
     host: true,
+    allowedHosts: ['chimaira-client.sliplane.app'],
     proxy:{
       '/api':{
         target: 'http://127.0.0.1:3001',
